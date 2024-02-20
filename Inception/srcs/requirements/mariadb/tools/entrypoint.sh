96234af3737eed83d33de249be35a4fd671b57d7
ShereeAnne <92857375+ShereeAnne@users.noreply.github.com>
2024-02-20 15:45:21 +0200

    update

--- Inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ Inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,25 +1,38 @@
 #!/bin/sh
 
-# Prepare directories and rights
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
+# make sure environment vars are set
+if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+  echo "Error: MARIADB_ROOT_PASSWORD environment variable not found."
+  exit 1
+fi
+if [ -z "$WORDPRESS_DB_USER" ] || [ -z "$WORDPRESS_DB_PASSWORD" ]; then
+  echo "Error: DB_USER or DB_USER_PASSWORD environment variable not found."
+  exit 1
+fi
+
+mkdir -p /var/lib/mysql /run/mysqld /var/log/mysql
 chown -R mysql:mysql /var/lib/mysql
-chmod -R 755 /var/lib/mysql
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/log/mysql
+touch /var/log/mysql/error.err
+chown -R mysql:mysql /var/log/mysql/error.err
 
-# init database
-mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+#give commands to database using root user, no password to begin with
+echo "Intializing database"
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --rpm >/dev/null 
 
-
+echo "Intializing database and users"
 mysqld --user=mysql --bootstrap << EOF
 USE mysql;
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-CREATE DATABASE $WORDPRESS_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '$WORDPRESS_DB_USER'@'%' IDENTIFIED by '$WORDPRESS_DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%';
-GRANT ALL PRIVILEGES ON *.* TO '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD' WITH GRANT OPTION;
-GRANT SELECT ON mysql.* TO '$WORDPRESS_DB_USER'@'%';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${WORDPRESS_DB_USER}'@'%' IDENTIFIED by '${WORDPRESS_DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_DB_USER}'@'%';
+GRANT ALL PRIVILEGES ON *.* TO '${WORDPRESS_DB_USER}'@'%';
+
 FLUSH PRIVILEGES;
 EOF
 
-exec mysqld --defaults-file=/etc/mariadb/50-server.conf
+exec mysqld_safe --defaults-file=/etc/my.cnf.d/50-server.cnf
--- Inception/srcs/requirements/wordpress/tools/config.sh
+++ Inception/srcs/requirements/wordpress/tools/config.sh
@@ -1,74 +1,34 @@
 #!/bin/sh
 
-#now add a waiting timer to wait for maria db
+while ! wget --spider -q mariadb:3306; do
+    echo "Waiting for database connection..."
+    sleep 5
+done
 
-if [ -f ./wordpress/wp-config.php ]
-then
-	echo "wordpress already downloaded"
+mkdir -p var/www/html/wordpress  /run/php/
+cd /var/www/html/
+if [ ! -f "/var/www/html/.wordpress_installed" ]; then
+	#Download wordpress files
+ 	wp core download --allow-root
+	#create config with wp-cli
+	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=mariadb --path=/var/www/html/ --force
+	# install wordpress
+	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --path=/var/www/html/ --allow-root
+	#Create user
+ 	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=editor --user_pass=$WP_USER_PASSWORD --path=/var/www/html/
+	# install themes and plugins
+	wp theme install bravada --activate --allow-root
+	wp plugin update --all
+ 	# 
+	wp option update siteurl "https://$DOMAIN_NAME"
+	wp option update home "https://$DOMAIN_NAME"
+	touch /var/www/html/.wordpress_installed
 else
-
-	wp core download --allow-root
-	
-	#wp create the config with wp config creat && wp core install ......
-	#wp user create and then set themes etc from here and set options
-	#Inport env variables in the config file
-	cd /var/www/html/wordpress
-	sed -i "s/username_here/$WORDPRESS_DB_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOST/g" wp-config-sample.php
-	sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" wp-config-sample.php
-	# mv wp-config-sample.php wp-config.php
+	echo "wordpress already downloaded and setup"
 fi
+
 chown -R www:www /var/www/html
 chmod -R 775 /var/www/html
+
+#Run php-fpm in foreground
 exec /usr/sbin/php-fpm81 -F
-
-
-
-# #!/bin/sh
-
-
-# # # download wordpress use WP-CLI instead
-# # wget https://wordpress.org/latest.tar.gz
-# # # unpack wordpress
-# # tar -xf latest.tar.gz
-# # download cli for wordpress
-# # download wordpress with wp-cli allowing the root user to do so
-
-# # while ! mysql -u $DB_USER -p $DB_USER_PASSWORD -h mariadb $DB_NAME; do
-# # 	sleep 5
-# # done
-
-# cd /var/www/html/
-# wp core download --allow-root
-
-# # change directory to where you want them first (may need to make one)
-
-
-# #setup with the wp-cli https://www.cloudways.com/blog/wp-cli-commands/
-# # make config
-# #skip check to be removed once database is connected and running
-# wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=mariadb --path=/var/www/html/ --force
-
-# # install wordpress
-# wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --path=/var/www/html/ --allow-root
-# # make user
-# # --path=/var/www/html maybe set as same location as volume
-# wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=editor --user_pass=$WORDPRESS_PASSWORD --path=/var/www/html/
-# # install themes and plugins
-# wp theme install pixl --activate
-# # activate stuff e.g. themes
-# # wp theme activate pixl
-# wp plugin update --all
-
-# wp option update siteurl "https://$DOMAIN_NAME"
-# wp option update home "https://$DOMAIN_NAME"
-
-# # while [ true ] 
-# # do
-# # 	sleep 1
-# # done
-# chown -R nginx:nginx /var/www/html
-# chmod -R 755 /var/www/html/wordpress
-
-# exec php-fpm81
