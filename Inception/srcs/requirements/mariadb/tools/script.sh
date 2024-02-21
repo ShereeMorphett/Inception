8068394f5067812e32745419accb00328fa415fa
sheree.morphett <sheree.morphett@gmail.com>
2024-02-21 18:12:47 +0200

    broked

--- Inception/srcs/requirements/mariadb/tools/script.sh
+++ Inception/srcs/requirements/mariadb/tools/script.sh
@@ -2,11 +2,11 @@
 
 # make sure environment vars are set
 if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
-  echo "Error: MARIADB_ROOT_PASSWORD environment variable is not set."
+  echo "Error: MYSQL_ROOT_PASSWORD environment variable is not set."
   exit 1
 fi
 if [ -z "$WORDPRESS_DB_USER" ] || [ -z "$MYSQL_ROOT_PASSWORD" ]; then
-  echo "Error: DB_USER and/ or DB_USER_PASSWORD environment variable is not set."
+  echo "Error: DB_USER and/ or MYSQL_ROOT_PASSWORD environment variable is not set."
   exit 1
 fi
 
@@ -14,8 +14,8 @@ fi
 mkdir -p /var/lib/mysql /run/mysqld /var/log/mysql
 chown -R mysql:mysql /var/lib/mysql
 chown -R mysql:mysql /run/mysqld
-chown -R mysql:mysql /var/log/mysql \
-touch /var/log/mysql/error.log
+chown -R mysql:mysql /var/log/mysql
+touch /var/log/mysql/error.err
 
 #give commands to database using root user, no password to begin with
 echo "Intializing database"
@@ -27,8 +27,8 @@ USE mysql;
 FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${WORDPRESS_DB_USER}';
-CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${WORDPRESS_DB_NAME}'@'%' IDENTIFIED by '${WORDPRESS_DB_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_USER} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${WORDPRESS_DB_USER}'@'%' IDENTIFIED by '${WORDPRESS_DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_DB_USER}'@'%';
 GRANT ALL PRIVILEGES ON *.* TO '${WORDPRESS_DB_USER}'@'%';
 
